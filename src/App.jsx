86f62787f84b528a3cf29a7d040d080dba8ac614
shaheer-waqar <shaheerwaqar02@gmail.com>
2024-08-05 23:08:49 +0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Componets/Home";
 import Product from "./Componets/Product";
-import AddToCard from "./Componets/AddToCard";
 
 function App() {
   const [count, setCount] = useState(0);
